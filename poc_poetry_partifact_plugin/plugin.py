b2ba81eac99084e2b71708d4e1a128cf5c98b688
timothestes <timothestes@gmail.com>
2023-04-20 12:58:35 -0700

    feat: add better error message

--- poc_poetry_partifact_plugin/plugin.py
+++ poc_poetry_partifact_plugin/plugin.py
@@ -45,7 +45,11 @@ class PocPartifactPlugin(ApplicationPlugin):  # type: ignore
             login(repository="aws", profile="amino-shared", role=None)
             io.write_line("<fg=green>aws codeartifact successfully configured</info>")
         except Exception as e:
-            io.write_error_line(f"<error>Failed to configure aws codeartifact: {e}</>")
+            import traceback
+
+            io.write_error_line(
+                f"<error>Failed to configure aws codeartifact: \n{traceback.format_exc()}</>"
+            )
 
     def _handle_post_command(
         self, event: ConsoleTerminateEvent, event_name: str, dispatcher: EventDispatcher
